0add772dfcc6179d2d2ed824df1f9d6ebe381755
Bartosz Iwiński <01bartosz0001@gmail.com>
2019-08-11 16:26:09 +0200

    Add scripts in script.js

---
+++ js/script.js
@@ -0,0 +1,44 @@
+	'use strict';
+
+	const titleClickHandler = function(event){
+	console.log('Link was clicked!');
+	const clickedElement = this;
+
+	  /* remove class 'active' from all article links  */
+	  
+	const activeLinks = document.querySelectorAll('.titles a.active');
+
+	for(let activeLink of activeLinks){
+		activeLink.classList.remove('active');
+	}
+
+	  /* add class 'active' to the clicked link */
+	  
+	clickedElement.classList.add('active');
+
+	  /* remove class 'active' from all articles */
+	  
+	const activeArticles = document.querySelectorAll('.posts .active');
+
+	for(let activeArticle of activeArticles){
+		activeArticle.classList.remove('active');
+	}
+
+	  /* get 'href' attribute from the clicked link */
+	  
+	const idOfClickedElement = clickedElement.getAttribute('href');
+
+	  /* find the correct article using the selector (value of 'href' attribute) */
+	  
+	const currentArticle = document.querySelector(idOfClickedElement);
+
+	  /* add class 'active' to the correct article */
+	  
+	currentArticle.classList.add('active');
+	}
+
+	const links = document.querySelectorAll('.titles a');
+
+	for(let link of links){
+	  link.addEventListener('click', titleClickHandler);
+	}
